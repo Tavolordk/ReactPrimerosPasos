91ff8273d50aaebeacabe424acecb998c8fa5ed3
Octavio Olea <oleaoctavio23@gmail.com>
2022-05-17 19:10:14 -0500

    Mi primer proyecto con React y Bootstrap.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+const imgSrc = "https://www.cinepremiere.com.mx/wp-content/uploads/2021/05/shrek-twitter-900x491.jpg";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <div className="card">
+        <img src={imgSrc} className="img-fluid" alt="shrek"/>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="www.google.com.mx" className="btn btn-primary">Go somewhere</a>
+          </div>
+      </div>
     </div>
   );
 }
